d10a1379b878367bfeb363872a8fbd9db00f4016
harksodje <harksodje@gmail.com>
2022-01-29 23:49:31 +0100

    final_stage

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Blackjack-Game
+# Blackjack-Game is a game in which a two or more random numbers were chosen and `sum`
+# if the `sum` is less than equal 20, the game is still active
+# if the `sum` equal 21, the gamer is a winner
+# if the `sum` is greater than 21, the gamer is a looser
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-let firstNumber = 10
-let secondNumber = 4;
-let sum = firstNumber + secondNumber;
-let cards = [firstNumber, secondNumber]
+let cards = []
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message ="";
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el")
@@ -11,17 +8,52 @@ let max_ = 11;
 let min_ = 2;
 let newCard_
 
+let player = {
+    name : "Per",
+    chips : 145,
+    sayHello: function (){
+        console.log("Hi! Adisa.")
+    }
+}
+
+
+let playerName = "Per";
+let playerChips = 145;
+let playerEl = document.querySelector("#player-el")
+
+playerEl.textContent = player.name+' : $' + player.chips;
 
 let messageEl = document.getElementById("message-el");
 
-const startGame = () => {
+
+const getRandomCard = () =>{
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
     
+    if (randomNumber === 1){
+        return11;
+    } else if (randomNumber > 10){
+        return 10;
+    } else {
+        return randomNumber;
+    }
+    
+}
+
+const startGame = () => {
+    isAlive = true;
+    let firstNumber = getRandomCard();
+    let secondNumber = getRandomCard();
+    cards = [firstNumber,secondNumber]
+    sum = firstNumber + secondNumber
     renderGame()
 }
 
 const renderGame = ()=>{
     //cards.reverse()
-    cardsEl.textContent = "Cards: " + cards[0] + " - " + cards[1];
+    cardsEl.textContent = "Cards: " ;
+    for (let i = 0; i < cards.length; i += 1) {
+        cardsEl.textContent += cards[i] + " ";
+    }
     sumEl.textContent = "Sum:" + " " + sum;
     if (sum < 21){
         message = "Do you want to draw a new card"
@@ -32,14 +64,17 @@ const renderGame = ()=>{
        message = "You are out of the game";
        isAlive  = false;
     }
-    messageEl.textContent = message;
+    messageEl.textContent = message;  
 }
 
 const newCard = () => {
-    //newCard_ = 6;
-    cards.push(newCard_)
-    //newCard_= Math.floor(Math.random()*(max_ - min_ + 1) + min_);
-    //secondNumber = Math.floor(Math.random()*(max_ - min_ + 1) + min_);
-    sum += newCard_;
-    renderGame()
-}
+    if (isAlive === true && hasBlackJack === false){
+        let card = getRandomCard();
+        newCard_ = card;
+        cards.push(card )
+        
+        sum += newCard_;
+        renderGame()
+        }
+    }
+    
